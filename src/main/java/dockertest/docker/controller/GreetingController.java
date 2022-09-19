20914ac1d115038c5b37881387281c3c2d2a603a
JimmyKristensen <siswanto@live.dk>
2022-09-19 10:54:17 +0200

    Hello now docker

--- src/main/java/dockertest/docker/controller/GreetingController.java
+++ src/main/java/dockertest/docker/controller/GreetingController.java
@@ -18,7 +18,7 @@ public class GreetingController {
 
     @GetMapping("/")
     public ResponseEntity<Greeting> greeting(){
-        Greeting greeting = new Greeting("Hello for the second time Azure");
+        Greeting greeting = new Greeting("Hello new Azure");
         greetingRepository.save(greeting);
         return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
     }
